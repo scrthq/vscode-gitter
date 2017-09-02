90c60581eb229d12ac7a0fee24a3e175784826e7
Nate Ferrell <nate@scrthq.com>
2017-09-01 17:53:03 -0700

    first push of local to GitHub

---
+++ extension.js
@@ -0,0 +1,160 @@
+"use strict";
+const vscode = require("vscode");
+const request = require("request");
+var fs = require('fs');
+var roomList = [];
+var configuration = vscode.workspace.getConfiguration('gitter');
+var token = configuration.get('token');
+var API_VERSION = '/v1';
+var BASE_URL = 'https://api.gitter.im' + API_VERSION;
+var API_ROOMS = '/rooms';
+var API_MESSAGES = '/chatMessages';
+class Gitter {
+    GetRoomList(callback, data) {
+        var params = '?access_token=' + token;
+        roomList.length = 0;
+        var __request = function(urls, callback) {
+            var results = {},
+                t = urls.length,
+                c = 0,
+                handler = function(error, response, body) {
+                    var url = response.request.uri.href;
+                    results[url] = { error: error, response: response, body: body };
+                    if (++c === urls.length) {
+                        callback(results);
+                    }
+                };
+            while (t--) {
+                request(urls[t], handler);
+            }
+        };
+        var urls = [BASE_URL + API_ROOMS + params];
+        __request(urls, function(responses) {
+            var url, response;
+            for (url in responses) {
+                // reference to the response object
+                response = responses[url];
+                // find errors
+                if (response.error) {
+                    console.log("Error", url, response.error);
+                    return;
+                }
+                // render body
+                if (response.body) {
+                    let r = JSON.parse(response.body);
+                    for (let i = 0; i < r.length; i++) {
+                        if (r[i].oneToOne == true) {
+                            roomList.push({ id: r[i].id, label: 'User: ' + r[i].name, description: r[i].user.username });
+                        } else {
+                            roomList.push({ id: r[i].id, label: 'Room: ' + r[i].name, description: r[i].topic });
+                        }
+                    }
+                }
+            }
+            callback && callback(data);
+        });
+    }
+    ApiCall(roomId, data) {
+        var that = this;
+        if (!this._statusBarItem) {
+            this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        }
+        request.post({
+            url: BASE_URL + API_ROOMS + '/' + roomId + API_MESSAGES,
+            formData: data
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                that._statusBarItem.text = "$(comment) Message sent successfully!";
+                that._statusBarItem.show();
+                setTimeout(function() { that._statusBarItem.hide(); }, 5000);
+            }
+        });
+    }
+    QuickPick() {
+        return vscode.window.showQuickPick(roomList, { matchOnDescription: true, placeHolder: 'Select a room' });
+    }
+    Send(roomId, data) {
+        var sendMsg = function(roomId, data) {
+            if (data) {
+                var g = new Gitter();
+                g.ApiCall(roomId, data);
+            }
+        };
+        // sending a message to a specified channel/user/group
+        if (roomId) {
+            sendMsg(roomId, data);
+        } else {
+            var gitter = new Gitter;
+            var pick = gitter.QuickPick();
+            pick.then(item => {
+                if (item) {
+                    roomId = item.id;
+                    data.text = item.id;
+                    sendMsg(roomId, data);
+                }
+            });
+        }
+    }
+    SendMessage() {
+        var options = {
+            prompt: "Please enter a message"
+        };
+        vscode.window.showInputBox(options).then(text => {
+            if (text) {
+                var data = {
+                    text: text
+                };
+                this.GetRoomList(this.Send, data);
+            }
+        });
+    };
+    SendSelection() {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        var selection = editor.selection;
+        var text = '```' + editor.document.getText(selection) + '```';
+        var data = {
+            text: text
+        };
+        this.GetRoomList(this.Send, data);
+    }
+    SendFile() {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        var text = '```' + editor.document.getText() + '```';
+        var data = {
+            text: text
+        };
+        this.GetRoomList(this.Send, data);
+    }
+    dispose() {}
+}
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    if (token) {
+        let gitter = new Gitter();
+        // send typed message
+        vscode.commands.registerCommand('gitter.gitterSendMsg', () => gitter.SendMessage());
+        // send selected text as a message
+        vscode.commands.registerCommand('gitter.gitterSendSelection', () => gitter.SendSelection());
+        // send current file
+        vscode.commands.registerCommand('gitter.gitterSendFile', () => gitter.SendFile());
+        // Add to a list of disposables which are disposed when this extension is deactivated.
+        context.subscriptions.push(gitter);
+    } else {
+        vscode.window.showErrorMessage('Please enter a token to use this extension. You can see your Personal Access Token here: https://developer.gitter.im/apps');
+    }
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
